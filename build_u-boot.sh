9855b67a2195b3743916f72ecf45c8e5b39774cc
irene <ireneyclin@msi.com>
2017-06-07 15:10:55 +0800

    add build scrip

---
+++ build_u-boot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+FSL_TOOLCHAIN_DIR=~/Yocto/L3/fsl-linaro-toolchain/bin/
+export ARCH=arm
+export CROSS_COMPILE=$FSL_TOOLCHAIN_DIR/arm-none-linux-gnueabi-
+export PATH="$PATH:$FSL_TOOLCHAIN_DIR"
+export CC="$FSL_TOOLCHAIN_DIR/arm-none-linux-gnueabi-gcc"
+export STRIP="$FSL_TOOLCHAIN_DIR/arm-none-linux-gnueabi-strip"
+export __ARM_PCS_VFP=1
+make distclean;
+make mx6q_sabresd_config
+make HOSTCC="$CC" HOSTSTRIP="$STRIP" CROSS_COMPILE="$CROSS_COMPILE" -j8
+
+#./mkimage_imx/mkimage -n ./mkimage_imx/mx6q_4x_mt41j128.cfg.cfgtmp \
+	-T imximage -e 0x17800000 -d u-boot.bin u-boot.imx
+
+#make "HOSTCC=ccache /usr/bin/gcc -B/usr/bin/ HOSTSTRIP=/usr/bin/strip CROSS_COMPILE=arm-none-linux-gnueabi- OPTFLAGS=-Os" all -j8
